2e7d2d36883f2da6273601368ab175517fb77f4d
邹焕鑫 <634448817@qq.com>
2020-02-15 19:30:58 +0800

    惯性滑动算法调整

--- app/src/main/java/com/gesture/zhx/zhxgesture/MainActivity.java
+++ app/src/main/java/com/gesture/zhx/zhxgesture/MainActivity.java
@@ -1,13 +1,71 @@
 package com.gesture.zhx.zhxgesture;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.annotation.UiThread;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.gesture.zhx.gesture.PointMagnifyShrinkUtils;
+import com.gesture.zhx.gesture.PointSlideUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FrameLayout m1F1;
+    private ImageView m1I1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        m1F1 = findViewById(R.id.m1_f1);
+        m1I1 = findViewById(R.id.m1_i1);
+
+        PointSlideUtils.getInstance().Slide(m1F1).setEventUpCallBack(new PointSlideUtils.EventUpCallBack() {
+            @Override
+            public void TopSlide(int value) {
+
+            }
+
+            @Override
+            public void BottomSlide(int value) {
+
+            }
+
+            @Override
+            public void LeftSlide(int value) {
+
+            }
+
+            @Override
+            public void RightSlide(int value) {
+
+            }
+
+            @Override
+            public void Inertia(double value) {
+                  UploadImage(value);
+            }
+        });
+
+
+    }
+
+    private double H;
+    private void UploadImage(double h){
+        ViewGroup.LayoutParams params = m1I1.getLayoutParams();
+        H = params.height;
+        params.width = 200;
+        params.height = (int) (H + h);
+        m1I1.setLayoutParams(params);
     }
 }
--- gesture/src/main/java/com/gesture/zhx/gesture/PointSlideUtils.java
+++ gesture/src/main/java/com/gesture/zhx/gesture/PointSlideUtils.java
@@ -1,10 +1,14 @@
 package com.gesture.zhx.gesture;
 
+import android.os.CountDownTimer;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 /**
  * 上滑
@@ -80,6 +84,17 @@ public class PointSlideUtils {
         return this;
     }
 
+    public PointSlideUtils Slide(FrameLayout frameLayout){
+        frameLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                OnTouchClick(v,event);
+                return true;
+            }
+        });
+        return this;
+    }
+
     private void OnTouchClick(View v, MotionEvent event){
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -119,7 +134,7 @@ public class PointSlideUtils {
                 } else {
                     if (eventUpCallBack != null) eventUpCallBack.TopSlide(h);
                 }
-                PostInertia(h);
+                PostInertiaTwo(h);
                 //左滑 右滑 x
                 int w = ux1 - dx1;
                 if (w > 0) {
@@ -144,7 +159,7 @@ public class PointSlideUtils {
         void LeftSlide(int value);
         void RightSlide(int value);
         @Deprecated
-        void Inertia(int value);
+        void Inertia(double value);
     }
 
     //惯性滚动算法
@@ -164,12 +179,69 @@ public class PointSlideUtils {
                     return;
                 }
                 double s = sleep * 10;
-                if (eventUpCallBack != null) eventUpCallBack.Inertia((int) s);
+                if (eventUpCallBack != null) eventUpCallBack.Inertia(s);
                 sleep = (long) (sleep * 0.9);
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    //惯性滚动算法第二版
+    private double a = 5000;
+    private double tz = 0.016666;
+    private double ageI,nowI;
+    private void PostInertiaTwo(int jl){
+        a = 5000;
+        System.out.println("手指滑行距离:"+jl);
+        Double time = Chufa(System.currentTimeMillis() - DownTime,1000); //毫秒级
+        System.out.println("手指滑行时间:"+time);
+        Double sleep = Chufa(jl,time); //平均速度即是最后一秒的瞬时速度
+        UploadViewTwo(sleep);
+    }
+    private void UploadViewTwo(final Double sleep){
+        System.out.println("sleep:"+sleep);
+        //滑行时间
+        double t = Chufa(sleep,a);
+        System.out.println("滑行时间："+t);
+        //总帧数
+        double sumzs = Chufa(t,tz);
+        System.out.println("总帧数:"+sumzs);
+        //自由滑行长度
+        double L = Chufa(sleep*sleep,(2*a));
+        System.out.println("自由滑行长度："+L);
+        //第一帧长度
+        double x1 = sleep*tz - Chufa((a*(tz*tz)),2);
+        if (eventUpCallBack != null) eventUpCallBack.Inertia(x1);
+        ageI = x1;
+        CountDownTimer countDownTimer = new CountDownTimer((long) (17*sumzs),17) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                nowI = ageI - a*(tz*tz);
+                double nowv = Chufa(nowI,tz);
+                if (nowv < Chufa(sleep,3)){
+                    a = 3000;
+                }else if (nowv < Chufa(sleep , 4)){
+                    a = 1500;
+                }else if (nowv < Chufa(sleep , 5)){
+                    a = 750;
+                }
+                if (eventUpCallBack != null) eventUpCallBack.Inertia(nowI);
+                ageI = nowI;
+            }
+
+            @Override
+            public void onFinish() {
+                cancel();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    public Double Chufa(double a,double b) {
+        //“0.00000000”确定精度
+        DecimalFormat dF = new DecimalFormat("0.00000000");
+        return Double.parseDouble(dF.format((float)a/b));
     }
 }
